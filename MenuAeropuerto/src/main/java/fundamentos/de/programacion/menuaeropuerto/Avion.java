ee8d9e5d2a57b13126a3d57db55ad13fcf9fb85f
SinnsykFG <143673271+SinnsykFG@users.noreply.github.com>
2023-10-06 11:23:34 -0300

    Avion.java

--- MenuAeropuerto/src/main/java/fundamentos/de/programacion/menuaeropuerto/Avion.java
+++ MenuAeropuerto/src/main/java/fundamentos/de/programacion/menuaeropuerto/Avion.java
@@ -6,14 +6,12 @@ import java.util.Random;
 
 public class Avion {
     int id;
-    String modelo;
     int cantidadMaxAsientos;
     int cantidadActualPasajeros;
     boolean estadoAvion;
 
-    public Avion(int id, String modelo, int cantidadMaxAsientos, int cantidadActualPasajeros, boolean estadoAvion) {
+    public Avion(int id, int cantidadMaxAsientos, int cantidadActualPasajeros, boolean estadoAvion) {
         this.id = id;
-        this.modelo = modelo;
         this.cantidadMaxAsientos = cantidadMaxAsientos;
         this.cantidadActualPasajeros = cantidadActualPasajeros;
         this.estadoAvion = estadoAvion;
@@ -22,7 +20,6 @@ public class Avion {
     public Avion(){
         Random rd = new Random();
         this.id = rd.nextInt(100)+1;
-        this.modelo = "Airbus 320";
         this.cantidadMaxAsientos = rd.nextInt(18)+162;
         this.cantidadActualPasajeros = rd.nextInt(this.cantidadMaxAsientos + 1);
         this.estadoAvion = true;
